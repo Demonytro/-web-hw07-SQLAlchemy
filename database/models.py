ebc6b12d559d3845c82046980204fc89ed038fd6
Dmytro Oseledko <d.oseledko@outlook.com>
2023-04-12 00:18:54 +0300

    query

--- database/models.py
+++ database/models.py
@@ -1,7 +1,5 @@
-from sqlalchemy import Column, String, Integer, ForeignKey, DateTime, func, event, Date
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy.ext.hybrid import hybrid_property
-
 
 Base = declarative_base()
 
@@ -9,44 +7,37 @@ Base = declarative_base()
 class Teacher(Base):
     __tablename__ = 'teachers'
     id = Column(Integer, primary_key=True)
-    first_name = Column(String(120))
-    last_name = Column(String(120))
-    email = Column(String(100))
-    phone = Column('cell_phone', String(120))
-    address = Column(String(120))
-    start_work = Column(Date, nullable=False)
-    students = relationship('Student', secondary='teachers_to_students', back_populates='teachers')
+    fullname = Column(String(120), nullable=False)
+
+
+class Group(Base):
+    __tablename__ = 'groups'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)
 
 
 class Student(Base):
     __tablename__ = 'students'
-    id = Column('id', Integer, primary_key=True)
-    first_name = Column(String(120))
-    last_name = Column(String(120))
-    email = Column(String(100))
-    phone = Column('cell_phone', String(120))
-    address = Column(String(120))
-    teachers = relationship('Teacher', secondary='teachers_to_students', back_populates='students')
+    id = Column(Integer, primary_key=True)
+    fullname = Column(String(120), nullable=False)
+    group_id = Column('group_id', ForeignKey('groups.id', ondelete='CASCADE'))
+    group = relationship('Group', backref='students')
 
 
-class TeacherStudent(Base):
-    __tablename__ = 'teachers_to_students'
-    id = Column('id', Integer, primary_key=True)
-    teacher_id = Column(Integer, ForeignKey('teachers.id', ondelete='CASCADE', onupdate='CASCADE'))
-    student_id = Column(Integer, ForeignKey('students.id', ondelete='CASCADE', onupdate='CASCADE'))
+class Discipline(Base):
+    __tablename__ = 'disciplines'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
+    teacher_id = Column('teacher_id', ForeignKey('teachers.id', ondelete='CASCADE'))
+    teacher = relationship('Teacher', backref='disciplines')
 
-#
-# class ContactPerson(Base):
-#     __tablename__ = 'contacts'
-#     id = Column('id', Integer, primary_key=True)
-#     first_name = Column(String(120))
-#     last_name = Column(String(120))
-#     email = Column(String(100))
-#     phone = Column('cell_phone', String(120))
-#     address = Column(String(120))
-#     student_id = Column(Integer, ForeignKey('students.id', ondelete='CASCADE'))
-#     student = relationship('Student', backref='contacts')
-#
-#     @hybrid_property
-#     def fullname(self):
-#         return self.first_name + ' ' + self.last_name
+
+class Grade(Base):
+    __tablename__ = 'grades'
+    id = Column(Integer, primary_key=True)
+    grade = Column(Integer, nullable=False)
+    date_of = Column('date_of', Date, nullable=True)
+    student_id = Column('student_id', ForeignKey('students.id', ondelete='CASCADE'))
+    discipline_id = Column('discipline_id', ForeignKey('disciplines.id', ondelete='CASCADE'))
+    student = relationship('Student', backref='grade')
+    discipline = relationship('Discipline', backref='grade')
